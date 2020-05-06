4b1d92a65a35c44459a10f6f00c1dd260e36679f
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-05-06 11:05:41 +0900

    first commit

---
+++ firstSpringBoot/README.md
@@ -0,0 +1,4 @@
+```
+$ mvn package
+$ java -jar -Dspring.profiles.active=local demo-0.1.jar
+```
---
+++ firstSpringBoot/src/main/java/com/multi/demo/SpringBootDoClassApplication.java
@@ -0,0 +1,13 @@
+package com.multi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDoClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDoClassApplication.class, args);
+    }
+
+}
